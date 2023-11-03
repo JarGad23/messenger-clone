cfc49317e07bfa415289822b5dc6f9bfdbfd0efe
JarGad23 <j.gad@m8b.pl>
2023-11-03 11:02:01 +0100

    Modified Button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import clsx from "clsx";
+import { cn } from "@/lib/utils";
 
 interface ButtonProps {
   type?: "button" | "submit" | "reset" | undefined;
@@ -26,7 +26,7 @@ const Button: React.FC<ButtonProps> = ({
       onClick={onClick}
       type={type}
       disabled={disabled}
-      className={clsx(
+      className={cn(
         "flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2",
         disabled && "opacity-50 cursor-default",
         fullWidth && "w-full",
