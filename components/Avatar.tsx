a740fe1965be057759b5e0204d25052449397afa
JarGad23 <j.gad@m8b.pl>
2023-11-05 12:34:20 +0100

    Removed console logs

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -8,8 +8,6 @@ interface AvatarProps {
 }
 
 const Avatar = ({ user }: AvatarProps) => {
-  console.log(user.image);
-
   return (
     <div className="relative">
       <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
