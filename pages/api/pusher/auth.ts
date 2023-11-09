b7397c11b202f146b88df8064ca4fece043110b6
JarGad23 <j.gad@m8b.pl>
2023-11-09 15:23:50 +0100

    Fix in pusher auth

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -13,7 +13,7 @@ export default async function handler(
     return res.status(401);
   }
 
-  const socketId = req.body.socketId;
+  const socketId = req.body.socket_id;
   const channel = req.body.channel_name;
   const data = {
     user_id: session.user.email,
